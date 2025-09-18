b53eefe8b90af4bae8378bc77e27508d0bbc6c1d
LWeerts <83358839+LWeerts@users.noreply.github.com>
2025-09-18 10:47:49 +0200

    Added test for delete BeerOrder endpoint

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerOrderControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerOrderControllerIT.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -140,5 +141,19 @@ class BeerOrderControllerIT {
                         .with(jwtRequestPostProcessor))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.customerRef", is(updatedCustomerRef)));
+    }
+
+    @Rollback
+    @Test
+    @Order(5)
+    void testDeleteBeerOrder() throws Exception {
+        var beerOrder = beerOrderRepository.findAll().getFirst();
+        mockMvc.perform(delete(BeerOrderController.BEER_ORDER_PATH_ID, beerOrder.getId())
+                .with(jwtRequestPostProcessor))
+                .andExpect(status().isNoContent());
+
+        mockMvc.perform(get(BeerOrderController.BEER_ORDER_PATH_ID, beerOrder.getId())
+                .with(jwtRequestPostProcessor))
+                .andExpect(status().isNotFound());
     }
 }
